826344a34e838bef0bd07d7fde188ff54d7c2a37
AnliYang <anli.face@gmail.com>
2015-11-03 20:08:47 -0600

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,7 +1,11 @@
+#connect program file to this test file
 require_relative '../current_age_for_birth_year.rb'
 
+#just a description
 describe "current_age_for_birth_year method" do
+  #another description
   it "returns the age of a person based on the year of birth" do
+    #here's the actual code for the test.  it sticks in 1984 for the local variable and checks if it gives the expected answer of 31
     age_of_person = current_age_for_birth_year(1984)
     
     expect(age_of_person).to eq(31)
